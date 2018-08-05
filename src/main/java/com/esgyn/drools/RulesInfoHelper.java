d0d79878ea6967823e69a8ad63d8608080eefa46
Justin Jarae <lawnjarae@gmail.com>
2018-08-05 16:24:49 -0400

    Moved the data objects to the rules file.

---
+++ src/main/java/com/esgyn/drools/RulesInfoHelper.java
@@ -0,0 +1,26 @@
+package com.esgyn.drools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RulesInfoHelper {
+
+	List<String> firedRules = new ArrayList<>();
+
+	public List<String> getFiredRules() {
+		return firedRules;
+	}
+
+	public void setFiredRules(List<String> firedRules) {
+		this.firedRules = firedRules;
+	}
+
+	public RulesInfoHelper() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "RulesInfoHelper [firedRules=" + firedRules + "]";
+	}
+}
